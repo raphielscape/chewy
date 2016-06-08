ce1e71b1f713c8c9b61c4a3e274981ce7a739430
Patrick Bellasi <patrick.bellasi@arm.com>
2016-06-08 18:40:04 +0100

    sched/tune: fix accounting for runnable tasks (4/5)
    The accounting for tasks into boost groups of different CPUs is currently
    broken mainly because:
    a) we do not properly track the change of boost group of a RUNNABLE task
    b) there are race conditions between migration code and acconting code
    
    This fixes accounting for exiting tasks by adding a dedicated call early
    in the do_exit() syscall, which disables SchedTune accounting as soon as a
    task is flagged PF_EXITING.
    
    This flag is set before the multiple dequeue/enqueue dance triggered
    by cgroup_exit() which is useful only to inject useless tasks movements
    thus increasing possibilities for race conditions with the migration code.
    The schedtune_exit_task() call does the last dequeue of a task from its
    current boost group. This is a solution more aligned with what happens in
    mainline kernels (>v4.4) where the exit_cgroup does not move anymore a dying
    task to the root control group.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -54,6 +54,8 @@
 #include <linux/writeback.h>
 #include <linux/shm.h>
 
+#include "sched/tune.h"
+
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 #include <asm/pgtable.h>
@@ -728,16 +730,9 @@ void do_exit(long code)
 	}
 
 	exit_signals(tsk);  /* sets PF_EXITING */
-<<<<<<< HEAD
 
-	sched_exit(tsk);
+	schedtune_exit_task(tsk);
 
-	if (tsk->flags & PF_SU) {
-		su_exit();
-	}
-
-=======
->>>>>>> 573f31e... sched: Revert HMP and some MSM specific features
 	/*
 	 * tsk->flags are checked in the futex code to protect against
 	 * an exiting task cleaning up the robust pi futexes.
