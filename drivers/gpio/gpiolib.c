7632be6a3ddaba87f758b77a326f444217ad6678
Francisco Franco <franciscofranco.1990@gmail.com>
2017-05-08 18:23:21 +0000

    gpio: stop dumping the stack every time a gpio isn't valid
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -65,9 +65,10 @@ static inline void desc_set_label(struct gpio_desc *d, const char *label)
  */
 struct gpio_desc *gpio_to_desc(unsigned gpio)
 {
-	if (WARN(!gpio_is_valid(gpio), "invalid GPIO %d\n", gpio))
+	if (!gpio_is_valid(gpio)) {
+		pr_err("invalid GPIO %d\n", gpio);
 		return NULL;
-	else
+	} else
 		return &gpio_desc[gpio];
 }
 EXPORT_SYMBOL_GPL(gpio_to_desc);
