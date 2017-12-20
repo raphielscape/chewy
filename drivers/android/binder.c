c8f75e4b560ac3610b149d75cee6bbe70c52e9d9
Martijn Coenen <maco@android.com>
2017-12-20 10:20:48 +0100

    binder: Use ERR_PTR for binder_new_node()
    To make it easier to return different values in future
    patches. Also added kerneldoc comment.
    
    Change-Id: Id9f5bc16fc1e95c6cdf18be9d5edf8e815764bcb
    Signed-off-by: Martijn Coenen <maco@android.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -1362,6 +1362,16 @@ static struct binder_node *binder_init_node_ilocked(
 	return node;
 }
 
+/**
+ * binder_new_node() - creates a new binder_node
+ * @proc:      process to create node in
+ * @fp:        corresponding flat_binder_object, may be NULL
+ *
+ * This function creates a node in process @proc, initializing it according to
+ * @fp (if it is provided).
+ *
+ * Return: the new node, or an ERR_PTR value if an error occurred.
+ */
 static struct binder_node *binder_new_node(struct binder_proc *proc,
 					   struct flat_binder_object *fp)
 {
@@ -1369,7 +1379,7 @@ static struct binder_node *binder_new_node(struct binder_proc *proc,
 	struct binder_node *new_node = kzalloc(sizeof(*node), GFP_KERNEL);
 
 	if (!new_node)
-		return NULL;
+		return ERR_PTR(-ENOMEM);
 	binder_inner_proc_lock(proc);
 	node = binder_init_node_ilocked(proc, new_node, fp);
 	binder_inner_proc_unlock(proc);
@@ -2481,8 +2491,8 @@ static int binder_translate_binder(struct flat_binder_object *fp,
 	node = binder_get_node(proc, fp->binder);
 	if (!node) {
 		node = binder_new_node(proc, fp);
-		if (!node)
-			return -ENOMEM;
+		if (IS_ERR(node))
+			return PTR_ERR(node);
 	}
 	if (fp->cookie != node->cookie) {
 		binder_user_error("%d:%d sending u%016llx node %d, cookie mismatch %016llx != %016llx\n",
@@ -4648,8 +4658,8 @@ static int binder_ioctl_set_ctx_mgr(struct file *filp)
 		context->binder_context_mgr_uid = curr_euid;
 	}
 	new_node = binder_new_node(proc, NULL);
-	if (!new_node) {
-		ret = -ENOMEM;
+	if (IS_ERR(new_node)) {
+		ret = PTR_ERR(new_node);
 		goto out;
 	}
 	binder_node_lock(new_node);
