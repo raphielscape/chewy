2ab540ec8ea85db02124a3153fadfccd97cf01c4
Ryuunosuke Yoshikawa <reikoyoshikawa@outlook.com>
2018-02-10 11:34:54 +0700

    sched : reverting the LB_MIN
    Coz this is the last thing I do before that data stall come
    Moar inspect rrrrrreeeeeeeeee

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -52,7 +52,7 @@ SCHED_FEAT(TTWU_QUEUE, true)
 
 SCHED_FEAT(FORCE_SD_OVERLAP, false) 
 SCHED_FEAT(RT_RUNTIME_SHARE, true)
-SCHED_FEAT(LB_MIN, true)
+SCHED_FEAT(LB_MIN, false)
 
 /*
 • Apply the automatic NUMA scheduling policy. Enabled automatically
