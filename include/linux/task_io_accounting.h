cd4978291dde26a0926b056d20c7c540cda9459e
Ryuunosuke Yoshikawa <reikoyoshikawa@outlook.com>
2018-02-12 00:52:53 +0700

    RaphiDerps : Heya Keks

--- include/linux/task_io_accounting.h
+++ include/linux/task_io_accounting.h
@@ -1,47 +1,114 @@
 /*
- * task_io_accounting: a structure which is used for recording a single task's
- * IO statistics.
- *
- * Don't include this header file directly - it is designed to be dragged in via
- * sched.h.
- *
- * Blame Andrew Morton for all this.
+ * Task I/O accounting operations
  */
+#ifndef __TASK_IO_ACCOUNTING_OPS_INCLUDED
+#define __TASK_IO_ACCOUNTING_OPS_INCLUDED
 
-struct task_io_accounting {
-#ifdef CONFIG_TASK_XACCT
-	/* bytes read */
-	u64 rchar;
-	/*  bytes written */
-	u64 wchar;
-	/* # of read syscalls */
-	u64 syscr;
-	/* # of write syscalls */
-	u64 syscw;
-	/* # of fsync syscalls */
-	u64 syscfs;
-#endif /* CONFIG_TASK_XACCT */
+#include <linux/sched.h>
 
 #ifdef CONFIG_TASK_IO_ACCOUNTING
-	/*
-	 * The number of bytes which this task has caused to be read from
-	 * storage.
-	 */
-	u64 read_bytes;
+static inline void task_io_account_read(size_t bytes)
+{
+	current->ioac.read_bytes += bytes;
+}
 
-	/*
-	 * The number of bytes which this task has caused, or shall cause to be
-	 * written to disk.
-	 */
-	u64 write_bytes;
+/*
+ * We approximate number of blocks, because we account bytes only.
+ * A 'block' is 512 bytes
+ */
+static inline unsigned long task_io_get_inblock(const struct task_struct *p)
+{
+	return p->ioac.read_bytes >> 9;
+}
 
-	/*
-	 * A task can cause "negative" IO too.  If this task truncates some
-	 * dirty pagecache, some IO which another task has been accounted for
-	 * (in its write_bytes) will not be happening.  We _could_ just
-	 * subtract that from the truncating task's write_bytes, but there is
-	 * information loss in doing that.
-	 */
-	u64 cancelled_write_bytes;
+static inline void task_io_account_write(size_t bytes)
+{
+	current->ioac.write_bytes += bytes;
+}
+
+/*
+ * We approximate number of blocks, because we account bytes only.
+ * A 'block' is 512 bytes
+ */
+static inline unsigned long task_io_get_oublock(const struct task_struct *p)
+{
+	return p->ioac.write_bytes >> 9;
+}
+
+static inline void task_io_account_cancelled_write(size_t bytes)
+{
+	current->ioac.cancelled_write_bytes += bytes;
+}
+
+static inline void task_io_accounting_init(struct task_io_accounting *ioac)
+{
+	memset(ioac, 0, sizeof(*ioac));
+}
+
+static inline void task_blk_io_accounting_add(struct task_io_accounting *dst,
+						struct task_io_accounting *src)
+{
+	dst->read_bytes += src->read_bytes;
+	dst->write_bytes += src->write_bytes;
+	dst->cancelled_write_bytes += src->cancelled_write_bytes;
+}
+
+#else
+
+static inline void task_io_account_read(size_t bytes)
+{
+}
+
+static inline unsigned long task_io_get_inblock(const struct task_struct *p)
+{
+	return 0;
+}
+
+static inline void task_io_account_write(size_t bytes)
+{
+}
+
+static inline unsigned long task_io_get_oublock(const struct task_struct *p)
+{
+	return 0;
+}
+
+static inline void task_io_account_cancelled_write(size_t bytes)
+{
+}
+
+static inline void task_io_accounting_init(struct task_io_accounting *ioac)
+{
+}
+
+static inline void task_blk_io_accounting_add(struct task_io_accounting *dst,
+						struct task_io_accounting *src)
+{
+}
+
 #endif /* CONFIG_TASK_IO_ACCOUNTING */
-};
+
+#ifdef CONFIG_TASK_XACCT
+static inline void task_chr_io_accounting_add(struct task_io_accounting *dst,
+						struct task_io_accounting *src)
+{
+	dst->rchar += src->rchar;
+	dst->wchar += src->wchar;
+	dst->syscr += src->syscr;
+	dst->syscw += src->syscw;
+	dst->syscfs += src->syscfs;
+}
+#else
+static inline void task_chr_io_accounting_add(struct task_io_accounting *dst,
+						struct task_io_accounting *src)
+{
+}
+#endif /* CONFIG_TASK_XACCT */
+
+static inline void task_io_accounting_add(struct task_io_accounting *dst,
+						struct task_io_accounting *src)
+{
+	task_chr_io_accounting_add(dst, src);
+	task_blk_io_accounting_add(dst, src);
+}
+#endif /* __TASK_IO_ACCOUNTING_OPS_INCLUDED */
