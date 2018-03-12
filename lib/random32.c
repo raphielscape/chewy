86281dcf49d70b23cdc97df45b08fa0e0c46f514
vicatz <vickky@programmer.net>
2018-03-12 23:41:12 +0700

    Revert "random: fully switch to Chacha20"
    This reverts commit d092f1f928d873e9910bf31812a04585f0094f09.

--- lib/random32.c
+++ lib/random32.c
@@ -38,7 +38,6 @@
 #include <linux/random.h>
 #include <linux/sched.h>
 #include <asm/unaligned.h>
-#include <linux/timer.h>
 
 #ifdef CONFIG_RANDOM32_SELFTEST
 static void __init prandom_state_selftest(void);
@@ -152,8 +151,7 @@ static u32 __extract_hwseed(void)
 {
 	unsigned int val = 0;
 
-	(void)(arch_get_random_seed_int(&val) ||
-	       arch_get_random_int(&val));
+	(void)(arch_get_random_int(&val));
 
 	return val;
 }
@@ -182,7 +180,7 @@ void prandom_seed(u32 entropy)
 	 * No locking on the CPUs, but then somewhat random results are, well,
 	 * expected.
 	 */
-	for_each_possible_cpu(i) {
+	for_each_possible_cpu (i) {
 		struct rnd_state *state = &per_cpu(net_rand_state, i);
 
 		state->s1 = __seed(state->s1 ^ entropy, 2U);
@@ -202,7 +200,7 @@ static int __init prandom_init(void)
 	prandom_state_selftest();
 
 	for_each_possible_cpu(i) {
-		struct rnd_state *state = &per_cpu(net_rand_state, i);
+		struct rnd_state *state = &per_cpu(net_rand_state,i);
 		u32 weak_seed = (i + jiffies) ^ random_get_entropy();
 
 		prandom_seed_early(state, weak_seed, true);
@@ -222,7 +220,7 @@ static void __prandom_timer(unsigned long dontcare)
 	u32 entropy;
 	unsigned long expires;
 
-	get_random_bytes(&entropy, sizeof(entropy));
+	erandom_get_random_bytes((char *)&entropy, sizeof(entropy));
 	prandom_seed(entropy);
 
 	/* reseed every ~60 seconds, in [40 .. 80) interval with slack */
@@ -234,6 +232,7 @@ static void __prandom_timer(unsigned long dontcare)
 
 static void __init __prandom_start_seed_timer(void)
 {
+	set_timer_slack(&seed_timer, HZ);
 	seed_timer.expires = jiffies + msecs_to_jiffies(40 * MSEC_PER_SEC);
 	add_timer(&seed_timer);
 }
@@ -244,6 +243,7 @@ static void __init __prandom_start_seed_timer(void)
  */
 static void __prandom_reseed(bool late)
 {
+	int i;
 	unsigned long flags;
 	static bool latch = false;
 	static DEFINE_SPINLOCK(lock);
@@ -265,7 +265,19 @@ static void __prandom_reseed(bool late)
 		goto out;
 
 	latch = true;
-	prandom_seed_full_state(&net_rand_state);
+
+	for_each_possible_cpu(i) {
+		struct rnd_state *state = &per_cpu(net_rand_state,i);
+		u32 seeds[4];
+
+		erandom_get_random_bytes((char *)&seeds, sizeof(seeds));
+		state->s1 = __seed(seeds[0],   2U);
+		state->s2 = __seed(seeds[1],   8U);
+		state->s3 = __seed(seeds[2],  16U);
+		state->s4 = __seed(seeds[3], 128U);
+
+		prandom_warmup(state);
+	}
 out:
 	spin_unlock_irqrestore(&lock, flags);
 }
