2fdc4644d4c5761d4cdf638839a16db0ee263d59
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-04-22 15:33:34 +0700

    blkdev: nyeh
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -517,7 +517,7 @@ struct request_queue {
 #define QUEUE_FLAG_DEFAULT	((0 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (0 << QUEUE_FLAG_ADD_RANDOM))
+				 (0 << QUEUE_FLAG_ADD_RANDOM)   |	\
 				 (1 << QUEUE_FLAG_SAME_FORCE))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((0 << QUEUE_FLAG_IO_STAT) |		\
