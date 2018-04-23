6f0cee048a296c84c46e33ead469fcbff729418d
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-04-23 08:34:41 +0700

    scripting: inb4 raphielscape
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- chewy/scripts/kernel.sh
+++ chewy/scripts/kernel.sh
@@ -27,7 +27,7 @@ export CLANG_TRIPLE="aarch64-linux-gnu-";
 export CLANG_TCHAIN="$HOME/CLANG/clang-4691093/bin/clang";
 export KBUILD_COMPILER_STRING="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')";
 
-export TCHAIN_PATH="$HOME/GNU/GCC7/bin/aarch64-linux-android-";
+export TCHAIN_PATH="$HOME/GNU/GCC7/bin/raphielscape-";
 export CROSS_COMPILE="${CCACHE} ${TCHAIN_PATH}";
 export DEFCONFIG="mido_defconfig";
 export ZIP_DIR="${KERNELDIR}/chewy/files/";
